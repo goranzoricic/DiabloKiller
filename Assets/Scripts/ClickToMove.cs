c84a5174da4f013a1b43c4273b0b808bb8a69ae8
Goran Zoricic <goran.zoricic@gmail.com>
2016-01-05 11:50:34 +0100

    Initial commit.

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+	public float MinDistance = 0.01f;
+	public float MovementSpeed = 3000f;
+
+	private NavMeshAgent navMeshAgent;
+
+
+	// Use this for initialization
+	void Start () {
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (navMeshAgent == null) {
+			return;
+		}
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetButton("Fire1")) {
+			if (Physics.Raycast(ray, out hit, 100))	{
+				if (!hit.collider.CompareTag("Player")) {
+					navMeshAgent.destination = hit.point;
+					navMeshAgent.Resume();
+				}
+			}
+		}
+
+		if (navMeshAgent.remainingDistance <= MinDistance) {
+			navMeshAgent.Stop();
+		}
+	} 
+}
