697f964374908280076ec863d2e625072544f98e
Goran Zoricic <goran.zoricic@gmail.com>
2016-01-05 20:06:27 +0100

    Recoded player movement to be a better clone of Diablo 3 movement.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -18,4 +18,20 @@ public abstract class Character : MonoBehaviour {
 
 	public abstract void onDeath();
 
+	protected float PathLength(NavMeshPath path) {
+		if (path.corners.Length < 2)
+			return 0;
+		
+		Vector3 previousCorner = path.corners[0];
+		float lengthSoFar = 0.0F;
+		int i = 1;
+		while (i < path.corners.Length) {
+			Vector3 currentCorner = path.corners[i];
+			lengthSoFar += Vector3.Distance(previousCorner, currentCorner);
+			previousCorner = currentCorner;
+			i++;
+		}
+		return lengthSoFar;
+	}
+
 }
