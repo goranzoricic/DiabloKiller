b7cf6771f3e0da919d9b3ecb77aeeedadd80ed54
Goran Zoricic <goran.zoricic@gmail.com>
2016-02-20 19:18:04 +0100

    Projectiles now selfdestruct after 20 meters.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 namespace DiabloKiller {
+
+
     public class Projectile : MonoBehaviour {
+        private Vector3 origin = new Vector3(0, 0, 0);
+        private const float MAX_PROJECTILE_RANGE = 20;
 
         // Use this for initialization
         void Start() {
-
+            origin = transform.position;
         }
 
         // Update is called once per frame
-        void Update() {
-
+        void Update() { 
+            float distanceTraveled = (transform.position - origin).magnitude;
+            if (distanceTraveled > MAX_PROJECTILE_RANGE) {
+                Destroy(gameObject);
+            }
         }
 
         public virtual void OnCollisionEnter(Collision hit) {
